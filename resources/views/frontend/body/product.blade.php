4929ddcc8cd1f3df38514fe07d36cb6520299374
Arefin21 <arefinshagor18@gmail.com>
2024-01-22 01:05:41 +0600

    Feat: Product Frontend Complete

--- resources/views/frontend/body/product.blade.php
+++ resources/views/frontend/body/product.blade.php
@@ -9,152 +9,32 @@
             </div>
         </div>		
         <div class="row">	
-            <!-- Single project Inner Start -->
-            <div class="col-md-3 col-sm-6 inner">
-                <div class="single-project-inner">
-                    <div class="project-thumb">
-                        <img src="{{asset('frontend/img/products/1.jpg')}}" alt=""/>
-                        <div class="project-thumb-overlay">
-                            <div class="project-icon">
-                                <a href="#"><i class="icofont-external-link"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="project-inner-desc">
-                        <h2><a href="#">HEAT TRANSFER LABELS</a></h2>
-                        <p>Heat Transfer Label is an excellent way of printing patterns on customized and personalized commodities, and printing patterns that contain full-color photos or images.</p>
-                    </div>
-                </div>
-            </div>
             
-                    <div class="col-md-3 col-sm-6 inner">
-                <div class="single-project-inner">
-                    <div class="project-thumb">
-                        <img src="{{asset('frontend/img/products/24.png')}}" alt=""/>
-                        <div class="project-thumb-overlay">
-                            <div class="project-icon">
-                                <a href="#"><i class="icofont-external-link"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="project-inner-desc">
-                        <h2><a href="#">Reflective Heat Transfer Label</a></h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua.</p>
-                    </div>
-                </div>
-            </div>
-            
-                <div class="col-md-3 col-sm-6 inner">
-                <div class="single-project-inner">
-                    <div class="project-thumb">
-                        <img src="{{asset('frontend/img/products/4.jpg')}}" alt=""/>
-                        <div class="project-thumb-overlay">
-                            <div class="project-icon">
-                                <a href="#"><i class="icofont-external-link"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="project-inner-desc">
-                        <h2><a href="#">Heat Transfer Highdency Label</a></h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua.</p>
-                    </div>
-                </div>
-            </div>
+        @php
+            $products = App\Models\Product::latest()->take(8)->get();
+       @endphp
+       <!-- Single project Inner Start -->
 
-            <div class="col-md-3 col-sm-6 inner">
-                <div class="single-project-inner">
-                    <div class="project-thumb">
-                        <img src="{{asset('frontend/img/products/2.jpg')}}" alt=""/>
-                        <div class="project-thumb-overlay">
-                            <div class="project-icon">
-                                <a href="#"><i class="icofont-external-link"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="project-inner-desc">
-                        <h2><a href="#">Garments Care Label</a></h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua.</p>
-                    </div>
-                </div>
-            </div>
+       @foreach ($products as $product)
 
-            <div class="col-md-3 col-sm-6 inner">
-                <div class="single-project-inner">
-                    <div class="project-thumb">
-                        <img src="{{asset('frontend/img/products/hang.png')}}" alt=""/>
-                        <div class="project-thumb-overlay">
-                            <div class="project-icon">
-                                <a href="#"><i class="icofont-external-link"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="project-inner-desc">
-                        <h2><a href="#">Hang Tags</a></h2>
-                        <p>Hang tags are beneficial for the details that are important to a customer such as sizing, pricing and brand. But they are so much more than that in today’s bustling retail industry.</p>
-                    </div>
-                </div>
-            </div>	
-
-        
-
-    
-            <div class="col-md-3 col-sm-6 inner">
-                <div class="single-project-inner">
-                    <div class="project-thumb">
-                        <img src="{{asset('frontend/img/products/21.png')}}" alt=""/>
-                        <div class="project-thumb-overlay">
-                            <div class="project-icon">
-                                <a href="#"><i class="icofont-external-link"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="project-inner-desc">
-                        <h2><a href="#">Barcode & price sticker</a></h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua.</p>
-                    </div>
-                </div>
-            </div>
-            
-                    <div class="col-md-3 col-sm-6 inner">
-                <div class="single-project-inner">
-                    <div class="project-thumb">
-                        <img src="{{asset('frontend/img/products/22.png')}}" alt=""/>
-                        <div class="project-thumb-overlay">
-                            <div class="project-icon">
-                                <a href="#"><i class="icofont-external-link"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="project-inner-desc">
-                        <h2><a href="#">Silica Gel</a></h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua.</p>
-                    </div>
-                </div>
-            </div>
-            
-                    <div class="col-md-3 col-sm-6 inner">
-                <div class="single-project-inner">
-                    <div class="project-thumb">
-                        <img src="{{asset('frontend/img/products/23.png')}}" alt=""/>
-                        <div class="project-thumb-overlay">
-                            <div class="project-icon">
-                                <a href="#"><i class="icofont-external-link"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="project-inner-desc">
-                        <h2><a href="#">Graphics Design Solution</a></h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua.</p>
-                    </div>
-                </div>
-            </div>
-            
+       <div class="col-md-3 col-sm-6 inner">
+           <div class="single-project-inner">
+               <div class="project-thumb">
+                   <img src="{{asset($product->image)}}" alt=""/>
+                   <div class="project-thumb-overlay">
+                       <div class="project-icon">
+                           <a href="#"><i class="icofont-external-link"></i></a>
+                       </div>
+                   </div>
+               </div>
+               <div class="project-inner-desc">
+                   <h2><a href="#">{{$product->title}}</a></h2>
+                   <p>{{$product->description}}</p>
+               </div>
+           </div>
+       </div>
+           
+       @endforeach
             
 
                                 
